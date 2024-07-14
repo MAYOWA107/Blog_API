4e87c0592c96e1df7f8061b23817b19b5816fc01
Mayowa <ogunkoyamayowa77@gmail.com>
2024-07-13 17:55:26 -0700

    blog

--- Blog/settings.py
+++ Blog/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-5bf_d6jq#x8uep6l72hwfx)bw!)-ywi0#42kin&ur9#qipk91a
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
